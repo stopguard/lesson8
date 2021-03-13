40d739a69ea509a6ad99a4f0a1b23eaa17569042
StopGuard <stopguard@bk.ru>
2021-03-14 01:29:35 +0500

    Answer for question 4

--- Question 4.py
+++ Question 4.py
@@ -23,20 +23,27 @@ import re
 
 
 def val_checker(verify):
+    """Сравнение с нулём"""
     def _cube(func):
+        """Получаем функцию"""
         def cube(number):
-            if not verify(number):
-                raise ValueError(f'wrong val {number}')
-            return func(number)
+            """Получаем аргументы и вызываем декорируемую функцию"""
+            print(f'Возводим в куб число: {number}')        # выводим последствия работы регулярки
+            if not verify(number):                              # если число не больше нуля
+                raise ValueError(f'wrong val {number}')             # вызываем исключение
+            return func(number)                                 # возвращаем результат работы калк_куба
         return cube
     return _cube
 
 
 @val_checker(lambda x: x > 0)
 def calc_cube(x):
+    """Вычисляем куб числа"""
     return x ** 3
 
 
-_value = int(''.join(re.findall(r'[-+]?[-+0-9]+', input('Введите положительное число для возведения в куб: '))))
-result = calc_cube(_value)
-print(result)
+_value = int(''.join(re.findall(r'[-+]?[0-9]+',
+                                input('Введите положительное число для возведения в куб'
+                                      '(лишние символы будут удалены): '))[0]))  # получаем число и фильтруем от мусора
+result = calc_cube(_value)          # получаем результат
+print(f'{_value}^3 = {result}')     # выводим
